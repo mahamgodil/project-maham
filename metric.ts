da36a549a441a532f967959a1688c326d36bbbbe
RomanczuG <romanczug@icloud.com>
2023-09-17 20:14:52 -0400

    Added metrics to github. Got github link for NPM

--- metric.ts
+++ metric.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+require('dotenv').config();
 
 const token = process.env.GITHUB_API_TOKEN;
 //const repositoryUrl = 'https://api.github.com/repos/nytimes/covid-19-data'; // must be in form https://api.github.com/repos/${Owner}/${Name}
@@ -8,70 +9,53 @@ const headers = {
     Authorization: `Bearer ${token}`,
 };
 
-//busFactor();
-//license();
-//correctness();
-//responsiveMaintainer();
-//rampUp();
 
-export async function busFactor(repositoryUrl:string) {
+export async function busFactor(repositoryUrl: string) {
   try {
-    // Fetch repository information
     const repositoryResponse = await axios.get(repositoryUrl, { headers });
     const repositoryData = repositoryResponse.data;
-
-    // Get the total number of commits in the repository
-    var totalCommits:number = 0;
-
-    // Fetch the list of contributors
+    let totalCommits = 0;
     const contributorsUrl = repositoryData.contributors_url;
     const contributorsResponse = await axios.get(contributorsUrl, { headers });
     const contributorsData = contributorsResponse.data;
 
-    // Calculate and count significant contributors
-    contributorsData.forEach(function (contributor:any) {
-        totalCommits += contributor.contributions;
-        //console.log(contributor.contributions);
-      });
-    //console.log("Total commits: " + totalCommits);
+    contributorsData.forEach((contributor: any) => {
+      totalCommits += contributor.contributions;
+    });
+
     const significantContributors = contributorsData.filter(
       (contributor: any) => (contributor.contributions / totalCommits) * 100 > 5
     );
 
-    //console.log(`Number of significant contributors: ${significantContributors.length}`);
+    console.log("BusFactor:", significantContributors.length);
     return significantContributors.length;
-  } catch (error:any) {
+  } catch (error: any) {
     console.error('Error:', error.message);
+    return -1;  // or throw the error if you want to handle it outside this function
   }
 }
 
-export async function license(repositoryUrl:string) {
+export async function license(repositoryUrl: string) {
   try {
-    const repositoryResponse = await axios.get(repositoryUrl, { headers });
-    const repositoryData = repositoryResponse.data;
-
-    const licenseUrl = repositoryUrl + "/license";
-    const LicenseResponse = await axios.get(licenseUrl);
-  
-    // If the repository has a license, the response status will be 200 OK.
-    // If the repository does not have a license, the response status will be 404 Not Found.
+    const licenseUrl = `${repositoryUrl}/license`;
+    const LicenseResponse = await axios.get(licenseUrl, { headers });
     if (LicenseResponse.status === 200) {
-      //console.log('The repository has a license.');
+      console.log('License: 1');
       return 1;
-    }
-    else {
+    } else {
       console.log(`Unexpected response status: ${LicenseResponse.status}`);
+      return -1;
     }
-  } catch (error:any) {
+  } catch (error: any) {
     if (error.response.status === 404) {
-      //console.log('The repository does not have a license.');
       return 0;
-    } 
-    else {
+    } else {
       console.log(`Unexpected response status: ${error.response.status}`);
+      return -1;
     }
   }
 }
+
 
 export async function correctness(repositoryUrl:string) {
   const repositoryResponse = await axios.get(repositoryUrl, { headers });
@@ -87,90 +71,89 @@ export async function correctness(repositoryUrl:string) {
   let totalClosedIssues = 0;
 
   // Function to recursively fetch all issues
-  async function fetchAllIssues(page: number = 1) {
+  async function fetchAllIssues(page: number = 1): Promise<number> {
     try {
       const response = await axios.get(issuesUrl, { params: { ...params, page }, headers });
       const issues = response.data;
       totalIssues += issues.length;
       totalClosedIssues += issues.filter((issue: any) => issue.state === 'closed').length;
 
-      // Fetch the next page if available
       const linkHeader = response.headers.link;
       if (linkHeader && linkHeader.includes('rel="next"')) {
         const nextPage = page + 1;
-        await fetchAllIssues(nextPage);
+        return await fetchAllIssues(nextPage);
       } else {
-        // All issues have been fetched, display the results
-        const bugPercentage = (totalClosedIssues / totalIssues) * 100;
-        //console.log(`Percentage of closed issues: ${bugPercentage.toFixed(2)}%`);
-        return bugPercentage.toFixed(5);
+        const bugPercentage = (totalClosedIssues / totalIssues);
+        console.log(`Correctness: ${bugPercentage.toFixed(5)}`);
+        return bugPercentage;
       }
     } catch (error) {
       console.error('Error making API request:', error);
+      return 0; // You might want to decide on a more appropriate default value
     }
   }
-  fetchAllIssues();
+
+  return await fetchAllIssues();
 }
 
-export async function responsiveMaintainer(repositoryUrl:string) {
-  const repositoryResponse = await axios.get(repositoryUrl, { headers });
-  const repositoryData = repositoryResponse.data;
+export async function responsiveMaintainer(repositoryUrl: string) {
 
-  const issuesUrl = repositoryUrl + "/issues?state=all";
+  const issuesUrl = `${repositoryUrl}/issues?state=all`;
   const params = {
     state: 'all',
-    per_page: 100, // Increase this value to retrieve more issues per page
-    page: 1, // Start with page 1
+    per_page: 100,
+    page: 1,
   };
   let totalIssues = 0;
   let totalResponseTime = 0;
-    
-  async function fetchAllIssues(page: number = 1) {
+
+  async function fetchCommentsForIssue(issue: any) {
+    try {
+      const response = await axios.get(issue.comments_url, { headers });
+      if (response.status === 200) {
+        const comments = response.data;
+        const maintainerComments = comments.filter((comment: any) => comment.user.type === 'User');
+        if (maintainerComments.length > 0) {
+          const firstMaintainerComment = maintainerComments[0];
+          const createdAt = new Date(issue.created_at);
+          const respondedAt = new Date(firstMaintainerComment.created_at);
+          const responseTime = respondedAt.getTime() - createdAt.getTime();
+          totalResponseTime += ((responseTime / 1000) / 60) / 60;
+          totalIssues++;
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching comments for issue:', error);
+    }
+  }
+
+  async function fetchAllIssues(page: number = 1): Promise<number> {
     try {
       const response = await axios.get(issuesUrl, { params: { ...params, page }, headers });
       const issues = response.data;
 
-      for (const issue of issues) {
-        const commentsEndpoint = issue.comments_url;
+      await Promise.all(issues.slice(0, 10).map(fetchCommentsForIssue));
 
-        try {
-          const response = await axios.get(commentsEndpoint, { headers });
-
-          if (response.status === 200) {
-            const comments = response.data;
-            const maintainerComments = comments.filter(
-              (comment: any) => comment.user.type === 'User');
-
-            if (maintainerComments.length > 0) {
-              const firstMaintainerComment = maintainerComments[0];
-              const createdAt = new Date(issue.created_at);
-              const respondedAt = new Date(firstMaintainerComment.created_at);
-              const responseTime = respondedAt.getTime() - createdAt.getTime();
-              totalResponseTime += ((responseTime / 1000) / 60) / 60; // milliseconds to minutes to hours
-              totalIssues++;
-            }
-          }
-        } catch (error) {
-          console.error('Error fetching comments for issue:', error);
-        }
+      const linkHeader = response.headers.link;
+      if (linkHeader && linkHeader.includes('rel="next"')) {
+        const nextPage = page + 1;
+        return await fetchAllIssues(nextPage);
+      } else {
+        const averageResponseTime = totalResponseTime / totalIssues / 100;
+        console.log(`ResponsiveMaintainer: ${averageResponseTime}`);
+        return averageResponseTime;
       }
-        // Fetch the next page if available
-        const linkHeader = response.headers.link;
-        if (linkHeader && linkHeader.includes('rel="next"')) {
-          const nextPage = page + 1;
-          await fetchAllIssues(nextPage);
-        } else {
-          // All issues have been fetched, display the results
-          const bugPercentage = totalResponseTime / totalIssues;
-          //console.log(`Average response time of issues: ${bugPercentage.toFixed(2)}`);
-          return bugPercentage.toFixed(5);
-        }
     } catch (error) {
       console.error('Error making API request:', error);
+      return 0; // You might want to decide on a more appropriate default value
     }
   }
-  fetchAllIssues();
+
+  return await fetchAllIssues();
 }
+
+
+
 
 export async function rampUp(repositoryUrl:string) {
   return -1;
